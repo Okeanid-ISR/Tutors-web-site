1db2ab9c3d880c77b5280af93b7b0889944f636a
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-02-26 17:14:14 +0200

    Feat: containers for width regulation created

---
+++ src/components/Middle.jsx
@@ -0,0 +1,6 @@
+const Middle = (props) => {
+    return <div className="w-3/5">{props.children}</div>
+
+}
+
+export default Middle;
---
+++ src/components/RightContainer.jsx
@@ -0,0 +1,6 @@
+const RightContainer = (props) => {
+    return <div className="w-1/5">{props.children}</div>
+
+}
+
+export default RightContainer;
